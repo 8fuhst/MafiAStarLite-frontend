a1a74cdaf25ed11e05ac2aa7ca5b65f36d2df0a0
8fuhst <59779937+8fuhst@users.noreply.github.com>
2023-07-11 15:02:11 +0200

    Images and grid-layout

--- src/components/SongBox.vue
+++ src/components/SongBox.vue
@@ -9,30 +9,44 @@
     props: ['song'],
     methods: {
       async getImage() {
-        this.image = await axios.get(this.$hostname + 'img?id=' + this.song.pk)
+        const response = await axios.get(
+            this.$hostname + "img?id=" + this.song.pk,
+            { responseType: "blob" }
+        );
+        this.image = URL.createObjectURL(response.data);
+      },
+    },
+    watch: {
+      async song(newSong) {
+        this.image = undefined
+        const response = await axios.get(
+            this.$hostname + "img?id=" + newSong.pk,
+            {responseType: "blob"}
+        );
+        this.image = URL.createObjectURL(response.data);
       }
     },
     created() {
-      this.getImage()
+      this.getImage();
     },
     data() {
       return {
-        image: []
+        image: undefined
       }
     }
   }
 </script>
 
 <template>
-  <div v-if="song" class="flex w-1/3 flex-wrap px-3 -mb-12">
-    <div class="w-full px-2">
-      <div class="relative overflow-hidden bg-cover bg-no-repeat" style="background-position: 50%">
-        <img class="block w-full h-full rounded-lg object-cover object-center" :src="this.image"/>
+  <div v-if="song" class="flex flex-wrap w-full">
+    <div class="relative w-full">
+      <div class="relative overflow-hidden bg-cover bg-no-repeat aspect-square scale-100 hover:scale-105 ease-in duration-150" style="background-position: 50%">
+        <img class="block w-full h-full rounded-lg object-cover object-center" :src="image"/>
         <div class="absolute rounded-lg bottom-0 right-0 left-0 top-0 h-full w-full bg-black bg-fixed opacity-50 overflow-hidden"></div>
-      </div>
-      <div class="relative inset-y-[-20%] bottom-5 hidden py-5 text-center text-white md:block">
-        <p class="text-xl">{{ song.fields.song_name }}</p>
-        <p class="text-base">{{ song.fields.song_artist }}</p>
+        <div class="absolute bottom-5 text-center text-white md:block w-full">
+          <p class="text-xl">{{ song.fields.song_name }}</p>
+          <p class="text-base">{{ song.fields.song_artist }}</p>
+        </div>
       </div>
     </div>
   </div>
